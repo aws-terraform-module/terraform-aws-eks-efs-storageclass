caa5c3247ca3166a336509b0e2f676fb2a6420c5
Mr Nim <50592567+mrnim94@users.noreply.github.com>
2024-12-16 08:56:12 +0000

    update provider

--- c1-versions.tf
+++ c1-versions.tf
@@ -12,7 +12,7 @@ terraform {
     }
     http = {
       source  = "hashicorp/http"
-      version = "~> 2.1"
+      version = "~> 3.0"
     } 
   }
   # Adding Backend as S3 for Remote State Storage
