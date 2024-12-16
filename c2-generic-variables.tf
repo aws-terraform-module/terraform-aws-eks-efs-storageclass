991c2c2af4846ce898f23d6cf0f68f5306a28c0b
Mr Nim <50592567+mrnim94@users.noreply.github.com>
2024-12-16 03:46:22 +0000

    add extra validation for throughput_mode variable

--- c2-generic-variables.tf
+++ c2-generic-variables.tf
@@ -71,4 +71,8 @@ variable "throughput_mode" {
   description = "(Optional) Throughput mode for the file system. Defaults to elastic. Valid values: `bursting`, or `elastic`."
   type = string
   default = "elastic"
+  validation {
+    condition     = contains(["bursting", "elastic"], var.throughput_mode)
+    error_message = "Allowed values for throughput_mode are 'bursting' or 'elastic'."
+  }
 }
